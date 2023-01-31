3a95007e3cc2578a29163303de8c8a79698a9c49
Somphong Phuangmalai <somphong.ph@gmail.com>
2023-01-31 21:56:42 +0700

    get by id

--- blog/get_by_id.go
+++ blog/get_by_id.go
@@ -1,1 +1,24 @@
 package blog
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type blogResponse struct {
+	Id string `json:"id"`
+}
+
+func (h *Handler) GetById(c echo.Context) error {
+	id := c.Param("id")
+	// var blog blogResponse
+
+	// Get data
+	blog, err := h.store.GetById(id)
+	if err != nil {
+		return c.JSON(http.StatusNotFound, "")
+	}
+
+	return c.JSON(http.StatusCreated, blog)
+}
